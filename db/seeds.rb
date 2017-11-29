8d047dd053e0cd6dabfeba4d83054f7cf6ee6abb
lucas hamani <hamanilucas@gmail.com>
2017-11-29 14:31:16 +0100

    adding restaurans and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding Starts"
+
+restaurant_name = ["Chez Camille", "Chez Lucas", "Chez Roger", "Chez Bernard", "Chez Ahmed"]
+
+address = ["1 rue de la soif", "2 rue de la faim", "3 rue de j'ai vraiment", "4 rue de la petite soeur", "5 rue du gravier"]
+
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+rating = rand(1..5)
+
+phone_number = ["+33", "+34", "+1", "+32", "+31"]
+
+content = ["Great!", "Top!", "I'll go there again!", "Delicious!", "Wonderful place!" ]
+
+10.times do
+  restaurant = Restaurant.new(name: restaurant_name.sample, address: address.sample, category: category.sample, phone_number: phone_number.sample)
+  restaurant.save
+  puts "#{restaurant.name} is saved!"
+end
+
+puts "Seeding done !"
